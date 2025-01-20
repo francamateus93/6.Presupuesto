abd4ba500ce56ce152a5044db86098ce8408c8b9
francamateus93 <francamateus@gmail.com>
2025-01-20 12:07:02 +0100

    Exercise 2 and 3 - Completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 
-function Card({ product, price, ischosen, onChange }) {
+function Card({ product, price, isChosen, onChange, children }) {
   return (
-    <div className="bg-white flex justify-between items-center gap-12 rounded-lg shadow-lg p-10 mx-auto">
+  <div className={`rounded-lg shadow-lg p-10 mx-auto" ${isChosen ? "border-2 border-gray-400" : "none"}`}>
+    <div className="flex justify-between items-center gap-10" >
       <div>
         <h3 className="text-2xl font-extrabold">{product}</h3>
         <p className="text-sm text-gray-600">Programming a complete responsive website</p>
       </div>
       <div className="flex items-center gap-5">
         <strong className="font-black text-3xl">{price}<span className="text-base">€</span></strong>
-       </div>
-       <div className="flex justify-center items-center gap-2">
+      </div>
+      <div className="flex justify-center items-center gap-2">
         <input 
           type="checkbox"
           name="input-form"
           className="form-checkbox h-4 w-4"
-          ischosen={ischosen}
+          checked={isChosen}
           onChange={onChange}
-        /> <span className="text-xs font-semibold text-gray-700">Check</span>
-        </div> 
+        />
+        <span className="text-xs font-semibold text-gray-700">Check</span>
       </div>
-  )
+      </div>
+      {isChosen && <div>{children}</div>}
+    </div>
+  );
 }
 
 export default Card
---
+++ src/components/Options.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Options({pages, setPages, languages, setLanguages}) {
+  const decreasesPage = () => setPages((prev) => (prev > 1 ? prev - 1 : 1))
+  const increasesPage = () => setPages((prev) => prev + 1)
+
+  const decreasesLanguage = () => setLanguages((prev) => (prev > 1 ? prev - 1 : 1))
+  const increasesLanguage = () => setLanguages((prev) => prev + 1)
+
+  return (
+      <div className="flex flex-col gap-2 items-end mt-4">
+        <div className="flex items-center justify-between gap-2">
+          <label htmlFor="pages" className="text-sm">Number of pages</label>
+          <button
+            className="px-2 rounded-full"
+            onClick={decreasesPage}>-</button>
+          <input 
+            type="number"
+            id="pages"
+            min="1"
+            value={pages}
+            className="border rounded-xl w-12 px-2 py-1 text-center text-sm"
+            onChange={(page) => setPages(Number(page.target.value))}
+          />
+          <button
+            className="px-2 rounded-full"
+            onClick={increasesPage}>+</button>
+        </div>
+        <div className="flex items-center justify-between gap-2">
+          <label htmlFor="languages" className="text-sm">Number of languages</label>
+        <button
+            className="px-2 rounded-full"
+            onClick={decreasesLanguage}>-</button>
+          <input 
+            type="number"
+            id="languages"
+            min="1"
+            value={languages}
+            className="border rounded-xl w-12 px-2 py-1 text-center text-sm"
+            onChange={(language) => setLanguages(Number(language.target.value))}
+          />
+          <button
+            className="px-2 rounded-full"
+            onClick={increasesLanguage}>+</button>
+        </div>
+      </div>
+  )
+}
+
+export default Options;
